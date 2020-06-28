fe8d845903599ee88b334aee25e81b0f528ef5a7
nakulgupta112233 <nakulgupta112233@gmail.com>
2020-06-28 00:47:39 -0700

    changed package.json
    Signed-off-by: nakulgupta112233 <nakulgupta112233@gmail.com>

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path')
 const app=express();
 var bodyParser = require('body-parser');
+
+const SERVER_PORT = process.env.PORT || 1111
 app.use(bodyParser.json());
 
 
@@ -12,7 +14,7 @@ app.use('/' , express.static(path.join(__dirname,'public')))
 //app.use('/a' , express.static(path.join(__dirname,'public_2')))
 app.use('/api',require ('./routes/api').route)
 
-app.listen(1111,()=>
+app.listen(SERVER_PORT,()=>
 {
     console.log("server started on https://localhost:1111");
 })
