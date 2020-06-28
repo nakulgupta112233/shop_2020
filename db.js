4e630481dc41e2663becb44d1af7fe28c14de6e8
nakulgupta112233 <67170847+nakulgupta112233@users.noreply.github.com>
2020-06-28 01:44:25 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,7 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize('d5shg1p83mopgh' , 'sbgalhjgnxdhzm' , 'df9ec1ae99ef65168f130e4d7bbcd2c5b2753e95d3d450c1171d7dc0e2381c5b' ,{
-  host: 'ec2-52-0-155-79.compute-1.amazonaws.com',
+const db = new Sequelize('postgres://sbgalhjgnxdhzm:df9ec1ae99ef65168f130e4d7bbcd2c5b2753e95d3d450c1171d7dc0e2381c5b@ec2-52-0-155-79.compute-1.amazonaws.com:5432/d5shg1p83mopgh' ,{
   dialect: 'postgres',
-  pool: {
-    min: 0,
-    max: 5,
-  }
-  //storage: __dirname + '/users.db'
 })
 
 const User = db.define('users', {
